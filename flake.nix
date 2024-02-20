0a8300a5c838921caf5b928b12f489830c067c0d
Sergei Zimmerman <sstsimmerman@gmail.com>
2024-02-20 21:24:59 +0300

    deps: update nixpkgs channel to nixos-23.11-small
    Bump nixpkgs to stable nixos-23.11-small channel. Update the revyos kernel
    and update the revyos_config to the latest provided by the thead-kernel.
    It differs from the default only by three flags, recommended by the original
    maintainers:
    
    CONFIG_DMIID=y
    CONFIG_VECTOR=n
    CONFIG_THEAD_ISA=n
    
    Use pkg-config instead of pkgconfig, since it's been renamed in newer nixpkgs.

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     # nixpkgs do not provide binary cache for riscv64-linux, we need to build everything from scratch anyway.
     # so we can use the small channel to get updates more quickly.
-    #    checkout more details here: https://hydra.nixos.org/jobset/nixos/release-23.05#tabs-jobs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05-small";
+    #    checkout more details here: https://hydra.nixos.org/jobset/nixos/release-23.11#tabs-jobs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11-small";
 
     # according to https://github.com/sipeed/LicheePi4A/blob/pre-view/.gitmodules
     thead-kernel = {
@@ -64,21 +64,21 @@
         ];
       });
 
-      light_aon_fpga = super.callPackage ./pkgs/firmware/light_aon_fpga.nix { };
-      light_c906_audio = super.callPackage ./pkgs/firmware/light_c906_audio.nix { };
-      thead-opensbi = super.callPackage ./pkgs/opensbi { };
+      light_aon_fpga = super.callPackage ./pkgs/firmware/light_aon_fpga.nix {};
+      light_c906_audio = super.callPackage ./pkgs/firmware/light_c906_audio.nix {};
+      thead-opensbi = super.callPackage ./pkgs/opensbi {};
     };
     pkgsKernelCross = import nixpkgs {
       localSystem = "x86_64-linux";
       crossSystem = buildFeatures;
 
-      overlays = [ overlay ];
+      overlays = [overlay];
     };
     pkgsKernelNative = import nixpkgs {
       localSystem = buildFeatures;
 
-      overlays = [ overlay ];
-  };
+      overlays = [overlay];
+    };
   in {
     # expose this flake's overlay
     overlays.default = overlay;
@@ -128,7 +128,7 @@
         targetPkgs = pkgs_: (with pkgs_;
           [
             # we need theses packages to run `make menuconfig` successfully.
-            pkgconfig
+            pkg-config
             ncurses
 
             pkgsKernelCross.gcc13Stdenv.cc
