05566c0aa333cbbe7e5e3ea1a46d7d40918d6ba5
Sergei Zimmerman <sstsimmerman@gmail.com>
2024-03-28 16:38:05 +0300

    chore(pkgs): switch to vendor uboot with sdcard support

--- pkgs/u-boot/default.nix
+++ pkgs/u-boot/default.nix
@@ -7,21 +7,19 @@
   version = "2023.08.06";
 
   src = fetchFromGitHub {
-    owner = "riscv-technologies-lab";
+    owner = "revyos";
     repo = "thead-u-boot";
-    rev = "43a2b1f1a1a06527f585ac504ecc78f42e0e6830";
-    sha256 = "sha256-kLr+MLOeQPpAVwvtjNUCHYVj2qBD8NwtBQXifgQEQKY=";
+    rev = "e0247b8a622a08256bb876fe706eedd3d5d645f7";
+    hash = "sha256-N5BTOUSdygOdBVH+rsgkrccf9BPKjKBZA/WWN1BOPQ8=";
   };
 
   defconfig = "light_lpi4a_defconfig";
 
-  extraMeta.platforms = ["riscv64-linux"];
-  extraMakeFlags = [
-    "OPENSBI=${thead-opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
-  ];
+  extraMeta.platforms = [ "riscv64-linux" ];
+  extraMakeFlags = [ "OPENSBI=${thead-opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin" ];
 
-  filesToInstall = ["u-boot-with-spl.bin"];
-})
-.overrideAttrs (oldAttrs: {
-  patches = []; # Remove all patches, which is not compatible with thead-u-boot
-})
+  filesToInstall = [ "u-boot-with-spl.bin" ];
+}).overrideAttrs
+  (oldAttrs: {
+    patches = [ ]; # Remove all patches, which is not compatible with thead-u-boot
+  })
